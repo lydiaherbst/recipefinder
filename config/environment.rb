9863af570f8bf7dcff730407e3895417b076944d
lydiaherbst <lherbst97@gmail.com>
2015-08-14 14:27:10 +0000

    new updates

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,14 @@ Bundler.require
 require './app/models/post'
 require './app/models/user'
 
+# configure :development do
+#   set :database, "sqlite3:db/database.db"
+# end
+
+configure :production do
+  ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"])
+end
+
 configure :development do
   set :database, "sqlite3:db/database.db"
 end
